360b2db418297b648e4c9a7d689080b9443aa104
annica <as@hawkingbros.com>
2024-03-21 21:42:15 +0300

    create footer

---
+++ shop/src/components/list/List.tsx
@@ -0,0 +1,16 @@
+import {ReactNode} from "react";
+
+interface ListProps<T>{
+    items: T[];
+    renderItem: (item: T) => ReactNode;
+    className?: string
+}
+
+export default function List<T>(props: ListProps<T>){
+    return(
+        <ul className={props.className}>
+            {props.items.map(props.renderItem)}
+        </ul>
+    )
+
+}
---
+++ shop/src/utils/const.tsx
@@ -0,0 +1,18 @@
+export const footerList = [
+    {
+        title: 'Избранное',
+        link: '/'
+    },
+    {
+        title: 'Корзина',
+        link: '/bag'
+    },
+    {
+        title: 'Контакты',
+        link: "tel:88005553535"
+    },
+    {
+        title: 'Условия сервиса',
+        link: '/'
+    },
+]
